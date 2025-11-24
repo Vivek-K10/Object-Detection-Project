ca69b0845c8b921bdd5292948020f7dd91bf08bf
Vivek-K10 <vivekaug11@gmail.com>
2025-11-24 15:41:48 +0530

    requirememts added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Object-Detection-Project
+
+## How to run:
+
+```bash
+conda create -n sinlang python=3.11 -y
+```
+
+```bash
+conda activate sinlang
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'signlanguage',
+    version='0.0.0',
+    author = 'vivek',
+    author_email='vivekaug11@gmail.com',
+    packages=find_packages(),
+    install_requires =[]
+)
